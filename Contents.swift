07f0bea5a99ee636e11d549aa081509b82220a62
Dylansweaver <dweaver@e112-m-14.mchenry.edu>
2016-09-22 16:29:07 -0500

    mathproblems

---
+++ Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "How much money is left after bills?"
+
+var StartingAmount:Double=500;
+let bill1:Double = 150;
+let bill2:Double = 75;
+let bill3:Double = 125;
+var taxes:Double = 0.92;
+var whatsLeft:Double = 0;
+var other1:Double = 0;//other expenses
+var other2:Double = 0;//other expenses
+var other3:Double = 0;//other expenses
+var savings:Double = 500;
+var forsavings:Double = 0.10;
+
+taxes = (StartingAmount * taxes)
+
+whatsLeft = taxes - bill1 - bill2 - bill3 - other1 - other2 - other3
+
+forsavings = whatsLeft * forsavings;
+
+savings = forsavings + savings;
+
+savings
+
+whatsLeft = whatsLeft-forsavings;
+
+
+
+
+
+
